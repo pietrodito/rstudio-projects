96c0b6c59b00e0ed37ccfeef2d1ced11ea9f9634
pietrodito <pietrodito@gmail.com>
2023-10-10 21:12:11 +0200

    Fix db setup, test if db already exist

--- Apps/dilavo/db_setup/setup-db.sh
+++ Apps/dilavo/db_setup/setup-db.sh
@@ -10,11 +10,22 @@ do
   sleep 1
 done
 
-createdb -h db MCO_DGF
-createdb -h db MCO_OQN
-createdb -h db HAD_DGF
-createdb -h db HAD_OQN
-createdb -h db SMR_DGF
-createdb -h db SMR_OQN
-createdb -h db PSY_DGF
-createdb -h db PSY_OQN
+
+if [ "$( psql -h db -XtAc \
+     "SELECT 1 FROM pg_database WHERE datname='PSY_OQN'" )" = '1' ]
+then
+    echo "Databases already exist"
+else
+    createdb -h db MCO_DGF
+    createdb -h db MCO_OQN
+    createdb -h db HAD_DGF
+    createdb -h db HAD_OQN
+    createdb -h db SMR_DGF
+    createdb -h db SMR_OQN
+    createdb -h db PSY_DGF
+    createdb -h db PSY_OQN
+fi
+
+
+
+top
