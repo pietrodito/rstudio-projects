15fa8246c98d10d02b3666f00613c646c1c12879
pietrodito <pietrodito@gmail.com>
2023-10-10 18:52:02 +0200

    Setup postgres, create one db for each nature

--- Apps/dilavo/db_setup/setup-db.sh
+++ Apps/dilavo/db_setup/setup-db.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+#
 
-psql -c "select * from DUAL"
+sleep 3
+
+createdb -h db MCO_DGF
+createdb -h db MCO_OQN
+createdb -h db HAD_DGF
+createdb -h db HAD_OQN
+createdb -h db SMR_DGF
+createdb -h db SMR_OQN
+createdb -h db PSY_DGF
+createdb -h db PSY_OQN
