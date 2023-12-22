9cd30686b463dd1bdbadcff21b28a7e49b4c1d21
pietrodito <pietrodito@gmail.com>
2023-12-22 09:14:58 +0100

    Fix minimal package install

--- INSTALL_MY_PACKAGES.R
+++ INSTALL_MY_PACKAGES.R
@@ -1,6 +1,21 @@
 setwd("~")
+
+follow_symlinks <- function(paths) {
+  
+  merge_path <- function(path, link) {
+    paste0(dirname(path), "/", link)
+  }
+  
+  (
+    paths
+    |> Sys.readlink() 
+    |> (\(x) ifelse(x == "", paths, merge_path(paths, x)))()
+  )
+}
+
 (
   "./my_packages/RStudio-tools"
   |>  fs::dir_ls()
+  |> follow_symlinks()
   |>  purrr::walk(~ devtools::install(.))
 )
